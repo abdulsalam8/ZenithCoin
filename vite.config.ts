f9856b84350575900e229de83854458cb32e33f5
abdulsalam8 <abdulsalamabubakar2023@gmail.com>
2024-05-31 15:12:51 +0100

    save

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react(), basicSsl()],
   build: {
-    outDir: 'build',
+    outDir: './docs'
   },
+  base: '/zenithcoin'
 });
