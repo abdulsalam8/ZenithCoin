8562cdb71dbdd460e15c9c340ecd08785824472f
abdulsalam8 <abdulsalamabubakar2023@gmail.com>
2024-05-31 15:09:30 +0100

    save

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl';
+// vite.config.js
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
   build: {
-    outDir: './docs'
+    outDir: 'build',
   },
-  base: '/vite-boilerplate/'
 });
